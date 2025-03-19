3bfac00b44e537fbf008fec05ee0d79f4b0e2d18
GabrielDlaco <gabriel.laco@aluno.senai.br>
2025-03-19 09:29:11 -0300

    first commit

---
+++ mudarCorDeFundo/script.js
@@ -0,0 +1,15 @@
+verde.addEventListener('click', function() {
+    document.body.style.backgroundColor = 'green';
+});
+
+vermelho.addEventListener('click', function() {
+    document.body.style.backgroundColor = 'red';
+});
+
+azul.addEventListener('click', function() {
+    document.body.style.backgroundColor = 'blue';
+});
+
+resetar.addEventListener('click', function() {
+    document.body.style.backgroundColor = 'white';
+});
